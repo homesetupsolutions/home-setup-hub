7c0e174c281af513a4ba2320defd46d8727c6e95
homesetupsolutions <admin@homesetupsolutions.ca>
2026-01-06 18:06:42 -0700

    Set BrowserRouter basename to '/home-setup-hub'

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/home-setup-hub">
             <ScrollToTop />
             <Routes>
               <Route path="/" element={<Index />} />
